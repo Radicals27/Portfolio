79ab089089493fa0cdbb63b97ba85cd7a4a1ef96
Conrad <conrad@maxart.com.au>
2024-10-18 10:42:35 +1000

    Fixed up grammar for consistency

--- projects.js
+++ projects.js
@@ -2,7 +2,7 @@
 export const projects = [
     {
         title: "Multiplayer Lunar VR Experience",
-        description: "(Unity, 2024) This was a multiplayer VR/MR co-located experience.",
+        description: "(Unity, VR, 2024) This was a multiplayer VR/MR co-located experience.",
         thumbnail: "./posters/Arose.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/Arose01.mp4", 
@@ -12,15 +12,15 @@ export const projects = [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/Arose05.mp4", 
         ],
         learnings: [
-            "Sequencer",
-            "Multiplayer",
-            "Colocation",
-            "Extremely fast project timeline (4 weeks)"
+            "Sequencer.",
+            "Multiplayer.",
+            "Colocation.",
+            "Extremely fast project timeline (4 weeks)."
         ]
     },
     {
         title: "Doping Control In Sports",
-        description: "(Unity, 2022, Quest & WebGL) An educational VR application designed to walk people through the process of doping control.",
+        description: "(Unity, VR & WebGL, 2022) An educational VR application designed to walk people through the process of doping control.",
         thumbnail: "./posters/DC.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/DC01.mp4", 
@@ -28,14 +28,14 @@ export const projects = [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/DC03.mp4",
         ],
         learnings: [
-            "Worked on both Android/Quest & WebGL (First time using WebGL)",
-            "Had very specific client requirements to follow",
-            "Designed & implemented the tutorial & sample jars pouring sequence",
+            "Worked on both Android/Quest & WebGL (First time using WebGL).",
+            "Had very specific client requirements to follow.",
+            "Designed & implemented the tutorial & sample jars pouring sequence.",
         ]
     },
     {
         title: "Ethical Decision Making In Sport",
-        description: "(Unity, 2020) A choose-your-own-adventure style training app to make athletes aware of the consequences of their choices.",
+        description: "(Unity, VR, 2020) A choose-your-own-adventure style training app to make athletes aware of the consequences of their choices.",
         thumbnail: "./posters/ED.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/ED01.mp4",
@@ -54,32 +54,32 @@ export const projects = [
     },
     {
         title: "Drone Combat",
-        description: "(Unity, 2022, Mobile/AR) A drone combat AR mobile game.",
+        description: "(Unity, Mobile/AR, 2022) A drone combat AR mobile game.",
         thumbnail: "./posters/DefyCombat.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/DefyCombat01.mp4", 
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/DefyCombat02.mp4"
         ],
         learnings: [
-            "Was principal developer",
-            "Worked closely with artists to generate a rapid prototype",
-            "Designed & Implemented drone AI behaviour as well as core gameplay loop",
+            "Was principal developer.",
+            "Worked closely with artists to generate a rapid prototype.",
+            "Designed & Implemented drone AI behaviour as well as core gameplay loop.",
         ]
     },
     {
         title: "Drone Racing",
-        description: "(Playcanvas, 2022) A drone racing game made in Playcanvas/JS.",
+        description: "(Playcanvas, Web, 2022) A drone racing game made in Playcanvas/JS.",
         thumbnail: "./posters/DefyTime.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/DefyTime01.mp4", 
         ],
         learnings: [
-            "First time using PlayCanvas",
+            "First time using PlayCanvas.",
         ]
     },
     {
         title: "Air Conditioner Advertisement",
-        description: "(Unity, 2023) A VR advertisement for the power of a particular air conditioner",
+        description: "(Unity, VR, 2023) A VR advertisement for the power of a particular air conditioner",
         thumbnail: "./posters/Nano.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/Nano01.mp4", 
@@ -87,30 +87,30 @@ export const projects = [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/Nano03.mp4",
         ],
         learnings: [
-            "Worked for a high-profile client with very specific needs",
-            "Rapid iteration times between change implementations and client feedback",
-            "Learned how to use Sequencer",
-            "Learned how to use VFX graph",
-            "Learned how to use Unity Timelines",
-            "Implemented all audio and sourced SFX",
+            "Worked for a high-profile client with very specific needs.",
+            "Rapid iteration times between change implementations and client feedback.",
+            "Learned how to use Sequencer.",
+            "Learned how to use VFX graph.",
+            "Learned how to use Unity Timelines.",
+            "Implemented all audio and sourced SFX.",
         ]
     },
     {
         title: "Overhead Awareness",
-        description: "(Unity, 2023) A mobile app to train electricians in how to be safe with overhead hazards.",
+        description: "(Unity, Mobile, 2023) A mobile app to train electricians in how to be safe with overhead hazards.",
         thumbnail: "./posters/OHA.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/OHA01.mp4", 
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/OHA02.mp4", 
         ],
         learnings: [
-            "Developed tools to allow clients to create their own scenarios more easily",
-            "Created several scenarios specific to client requirements",
+            "Developed tools to allow clients to create their own scenarios more easily.",
+            "Created several scenarios specific to client requirements.",
         ]
     },
     {
         title: "Servonauts prototyping",
-        description: "(Unity, 2023) Created various prototypes for conveyor belts and hazards for Servonauts game.",
+        description: "(Unity, Desktop & Switch, 2023) Created various prototypes for conveyor belts and hazards for Servonauts game.",
         thumbnail: "./posters/Servonauts.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/Servo01.mp4", 
@@ -118,15 +118,15 @@ export const projects = [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/Servo03.mp4",
         ],
         learnings: [
-            "Experimented with various obstacles such as oil slicks and conveyor belts",
-            "Prototyped several levels",
-            "Implemented 'juice' such as screen shake, dust particles and more",
-            "Implemented player spawn 'skybeam' effect",
+            "Experimented with various obstacles such as oil slicks and conveyor belts.",
+            "Prototyped several levels.",
+            "Implemented 'juice' such as screen shake, dust particles and more.",
+            "Implemented player spawn 'skybeam' effect.",
         ]
     },
     {
         title: "High Voltage Switching",
-        description: "(Unity, 2022) A mobile training application for electricians.",
+        description: "(Unity, Mobile, 2022) A mobile training application for electricians.",
         thumbnail: "./posters/Switching.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/Switching01.mp4", 
@@ -134,14 +134,14 @@ export const projects = [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/Switching03.mp4",
         ],
         learnings: [
-            "Further developed a tool that allows the client to create their own scenarios and apps",
-            "Designed and implemented an LRS-integration system to track user progress through the app, and update LRS on completion",
-            "Created a user-facing GUI to allow configuration of LRS system",
+            "Further developed a tool that allows the client to create their own scenarios and apps.",
+            "Designed and implemented an LRS-integration system to track user progress through the app, and update LRS on completion.",
+            "Created a user-facing GUI to allow configuration of LRS system.",
         ]
     },
     {
         title: "Test & Verify",
-        description: "(Unity, 2021) A mobile training application for electricians, particularly to do with electrical testing.",
+        description: "(Unity, Mobile, 2021) A mobile training application for electricians, particularly to do with electrical testing.",
         thumbnail: "./posters/TNV.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/TNV01.mp4", 
@@ -150,23 +150,23 @@ export const projects = [
         ],
         learnings: [
             "Created an app that trains electricians in how to test and verify electrical setups in people's homes.",
-            "Implemented a chaperone character system to walk the user through the app",
-            "Created the audio system to play SFX, VO and ambience",
-            "Worked closely with artists to request and implement new art assets",
+            "Implemented a chaperone character system to walk the user through the app.",
+            "Created the audio system to play SFX, VO and ambience.",
+            "Worked closely with artists to request and implement new art assets.",
         ]
     },
     {
         title: "Safety Alarm Advertisement",
-        description: "(Unity, 2022) A VR 360 video experience.",
+        description: "(Unity, VR, 2022) A VR 360 video experience.",
         thumbnail: "./posters/Tunstall.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/Tunstall01.mp4", 
         ],
         learnings: [
-            "Assisted with filming actors",
-            "Principal developer on project",
-            "First time using 360 video",
-            "Created Audio system and directional arrow system to guide user to face correct direction",
+            "Assisted with filming actors.",
+            "Principal developer on project.",
+            "First time using 360 video.",
+            "Created Audio system and directional arrow system to guide user to face correct direction.",
         ]
     },
     ,
@@ -188,7 +188,7 @@ export const projects = [
     },
     {
         title: "Work From Heights",
-        description: "(Unity, 2022-2024) A VR and iOS application to assist warehouse drivers with forklfit and LLOP safety.",
+        description: "(Unity, VR & iOS, 2022-2024) A VR and iOS application to assist warehouse drivers with forklfit and LLOP safety.",
         thumbnail: "./posters/WFH.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/WFH01.mp4", 
@@ -212,7 +212,7 @@ export const projects = [
     },
     {
         title: "Low Level Order Picker Training",
-        description: "(Unity, 2024) LLOP training for drivers in the warehouse.",
+        description: "(Unity, VR & iOS, 2024) LLOP training for drivers in the warehouse.",
         thumbnail: "./posters/LLOP.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/LLOP01.mp4", 
@@ -227,7 +227,7 @@ export const projects = [
     },
     {
         title: "Internet Of Things Designer",
-        description: "(Unity, 2020) A mobile app to assist laying out devices around one's home for IoT.",
+        description: "(Unity, Mobile, 2020) A mobile app to assist laying out devices around one's home for IoT.",
         thumbnail: "./posters/Neca.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/Neca01.mp4", 
@@ -237,12 +237,12 @@ export const projects = [
         ],
         learnings: [
             "First AR app I worked on.",
-            "Drew on a database of appliances the user could place around their home",
+            "Drew on a database of appliances the user could place around their home.",
             "Implemented scenarios where the user had to place items in appropriate places and be marked on the quality of this."
         ]
     },
     {
-        title: "VR game (Under NDA - Quest, Pico, Vive, PSVR, PCVR)",
+        title: "VR game (Under NDA - Quest, Pico, Vive, PSVR 1/2, PCVR)",
         description: "Worked on a popular VR title that has sold over a million copies worldwide.  Assisted with porting to various headsets as well as updated the EULA across each headset available including PCVR, Quest, Pico, PSVR1, PSVR2 & Vive.",
         thumbnail: "./img/VR.jpg",
         images: [
@@ -258,7 +258,7 @@ export const projects = [
     },
     {
         title: "Pete Simulator",
-        description: "(Unity, 2021, WebGL) A present for Secret Santa that I made for my boss, Pete.",
+        description: "(Unity, WebGL, 2021) A present for Secret Santa that I made for my boss, Pete.",
         thumbnail: "./posters/PeteSim.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/PeteSim01.mp4", 
@@ -272,7 +272,7 @@ export const projects = [
     },
     {
         title: "Alien Standup",
-        description: "(Unity, 2020-2021, Android & WebGL) A mobile game that is a mix between mad libs and Mastermind.  Try to figure out the Alien language via stand up comedy.",
+        description: "(Unity, Android & WebGL, 2020-2021) A mobile game that is a mix between mad libs and Mastermind.  Try to figure out the Alien language via stand up comedy.",
         thumbnail: "./posters/AlienStandup.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/AlienStandup01.mp4", 
@@ -282,13 +282,13 @@ export const projects = [
             "The first game I released to the public; on Play store & itch.io.",
             "Worked with an artist overseas to source sprites and animate them.",
             "Tracked player progress through the app using local storage.",
-            "Created database of words for player to play with",
+            "Created database of words for player to play with.",
             "Went through entire project lifecycle from idea, to implementation, to store upload."
         ]
     },
     {
         title: "Danny's Defense",
-        description: "(Unity, 2022) Another Secret Santa gift I made for a work colleague - Danny.  Up to 16 players defend the lunch room from garlic (which he was allergic  to), whilst using all of his favourite tools, a ping pong paddle, a guitar, tea, a laptop and ginger beer grenades.",
+        description: "(Unity, Desktop, 2022) Another Secret Santa gift I made for a work colleague - Danny.  Up to 16 players defend the lunch room from garlic (which he was allergic  to), whilst using all of his favourite tools, a ping pong paddle, a guitar, tea, a laptop and ginger beer grenades.",
         thumbnail: "./posters/DannyDefense.png",
         videos: [
             "https://github.com/Radicals27/Portfolio/raw/refs/heads/master/video/DannyD01.mp4", 
@@ -298,7 +298,7 @@ export const projects = [
         ],
         learnings: [
             "First time exploring multiplayer.",
-            "Used Photon/Pun package",
+            "Used Photon/Pun package.",
             "Learned how to use Blender to model, texture and rig a human model as well as a guitar, ping pong paddle, mug, laptop, ginger beer and garlic characters.",
             "100+ hour project, for fun.",
             "Learned a lot about project structure and how to plan for performance."
